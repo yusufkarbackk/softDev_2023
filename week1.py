3b97022216ec28a956c3ac6d37ebc8caaa824f14
Yusuf karback <yusufkarback64@gmail.com>
2023-03-06 20:15:05 +0700

    week 1

---
+++ week1.py
@@ -0,0 +1,13 @@
+print("hello softdev")
+
+# ini komen 
+pesan = "selamat pagi" #string
+print(pesan)
+print(f"isi pesan ini adalah: {pesan}")
+
+
+umur = 5  # variable dengan nama x yang mempunyai nilai = 5
+umurUserKamu = 10 #int
+
+if umur > 7:
+    print('x adalah bilangan positif')
