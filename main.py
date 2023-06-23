0b3dbdf238fe2cb1cf07c85e674442595600e856
Yusuf karback <yusufkarback64@gmail.com>
2023-06-24 00:23:51 +0700

    update

--- main.py
+++ main.py
@@ -27,6 +27,17 @@ def detail(menu_id):
     print(output_json)
     return render_template('detail_page.html', menu=output_json)
 
+
+@app.route("/delete/<int:menu_id>")
+def delete(menu_id):
+    db = getMySqlConnection()  # fungsi yang dipanggil sesuai dengan url diatas
+    sqlstr = f"DELETE from menu where id = {menu_id}"
+    cur = db.cursor()
+    cur.execute(sqlstr)
+    db.commit()
+    cur.close()
+    return redirect(url_for('index'))
+
 # rute yang di akses di url
 
 
@@ -37,6 +48,8 @@ def tambah_menu():  # fungsi yang dipanggil sesuai dengan url diatas
         data = request.form.to_dict()
         file = request.files['gambar_menu']
         data['gambar_menu'] = file.filename
+        if file == None:
+            print("WKWKWK")
         print(data)
         try:
             cur = db.cursor()
@@ -54,6 +67,55 @@ def tambah_menu():  # fungsi yang dipanggil sesuai dengan url diatas
             db.close()
         return redirect(url_for('index'))
     return render_template('add_menu.html')
+
+
+@app.route("/update/<int:menu_id>", methods=['POST', 'GET'])
+def update(menu_id):  # fungsi yang dipanggil sesuai dengan url diatas
+    db = getMySqlConnection()
+
+    if request.method == "POST":
+        data = request.form.to_dict()  # ambil data dari form
+
+        gambarMenu = request.files['gambar_menu']  # ambil data gambar
+
+        try:
+            cur = db.cursor()
+
+            if gambarMenu.filename:  # jika gambar di update
+                print("true")
+
+                # update nama gambar di database
+                cur.execute(
+                    f"UPDATE menu SET alamat_gambar = '{gambarMenu.filename}'")
+                db.commit()
+
+                # save gambar di folder images
+                gambarMenu.save(os.path.join(
+                    'static/images', gambarMenu.filename))
+
+            sqlstr = f"UPDATE menu SET nama_menu = '{data['nama_menu']}', deskripsi = '{data['deskripsi']}' WHERE id = {menu_id}; "
+
+            cur.execute(sqlstr)
+
+            db.commit()
+            cur.close()
+
+            print('sukses')
+            print(data)
+        except Exception as e:
+            print("Error in SQL:\n", e)
+        finally:
+            db.close()
+        return redirect(url_for('index'))
+
+    else:
+        sqlstr = f"SELECT * from menu where id = {menu_id}"
+        cur = db.cursor()
+        cur.execute(sqlstr)
+        output_json = cur.fetchall()
+
+        print(output_json)
+        return render_template('update_form.html', data=output_json)
 
 
 @app.route("/about")
